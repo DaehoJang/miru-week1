8b11b22d29c4549f09c06175a8458830152dfff5
Daeho Jang <dhjang4522@gmail.com>
2023-09-22 20:07:35 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,161 @@
+//fn main() {
+  //  let x = 5;
+    //let x= x + 1;
+    //let x = x * 2;
+    //println!("The Value of x is: {}", x);
+
+//}
+//fn main() {
+  // let guess: u32 = "42".parse().expect("Not a numebr"); 
+//}
+//fn main() {
+    //let a = [1, 2, 3, 4, 5];
+    //let index = 4;
+
+    //let element = a[index];
+
+    //println!("The value of element is: {}", element);
+//}
+//fn main() {
+    //println!("Hello, world");
+   // another_function();
+//}
+//fn another_function() {
+    //println!("another function")
+//}
+//fn main() {
+  //  another_function(5);
+//}
+//fn another_function(x:i32) {
+  //  println!("The Value of x is: {}",x);
+//}
+//fn main() {
+  //  another_function(5,6);
+//}
+//fn another_function(x:i32, y:i32) {
+  //  println!("The Value of x is: {}",x);
+    //println!("The Value of y is: {}",y);
+//}
+//fn main() {
+  //  let x = 5;
+    //let y = {
+      //  let x = 3;
+        //x + 1
+    //};
+    //println!("The Value of y is:{}", y);    
+//}
+
+//fn five() -> i32 {
+  //  5
+//}
+//fn main() {
+  //  let x = five();
+    //println!("The Value of x is:{}",x);
+//}
+//fn main() {
+  //  let x = plus_one(5);
+
+//    println!("The value of x is: {}", x);
+//}
+
+//fn plus_one(x: i32) -> i32 {
+  //  x + 1;
+//}
+
+//fn main() {
+    //let number = 3;
+    //if number < 5 {
+      //  println!("condition was true");
+    //} else {
+      //  println!("condition was false")
+    //}
+//}
+
+//fn main() {
+  //  let number = 3;
+    //if number {
+      //  println!("number was three");
+    //}
+//}
+
+//fn main() {
+//    let number = 5;
+  //  if number != 0 {
+    //    println!("number was something other than zero");
+//}
+//}
+
+//fn main() {
+    //let number = 6;
+    //if number % 4 == 0 {
+    //    println!("number is divisible by 4");
+    //} else if number % 3 == 0 {
+    //    println!("number is divisible by 3");
+    //} else if number % 2 == 0 {
+      //  println!("number is divisible by 2");
+    //} else {
+    //    println!("number is not divisible by 4, 3, or 2")
+  //  }
+//}
+
+//fn main() {
+//    let condition = true;
+//   let number = if condition {
+//        5
+//    } else {
+//        6
+//    };
+//    println!("The Value of number is:{}",number);
+//}
+
+//fn main() {
+//    let condition = true;
+//
+//    let number = if condition {
+//       5
+//    } else {
+//        "six"
+//    };
+//
+//    println!("The value of number is: {}", number);
+//}
+
+//      [Loops]
+
+//fn main() {
+//    loop {
+//        println!("again");
+//    }
+//}
+
+//fn main() {
+//    let mut number = 3;
+//    while number != 0 {
+//        println!("{}", number);
+//        number = number - 1;
+//    }
+//   println!("LIFTOFF!!");
+//}
+
+//fn main() {
+//    let a = [10,20,30,40,50];
+//    let mut index = 0;
+//    while index < 5 {
+//        println!("the value is:{}",a[index] );
+//        index = index + 1;
+//    }
+//}
+//
+//fn main() {
+//    let a = [10,20,30,40,50];
+//    for element in a.iter() {
+//        println!("the value is:{}",element);
+//    }
+//}
+
+//fn main() {
+//    for number in (1..4).rev() {
+//        println!("{}!", number);
+//    }
+//    println!("LIFTOFF!!!");
+//}
